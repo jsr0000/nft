adbfbae26bb9a2b960b717c8bf200b58f9e6dd2e
jsr0000 <josh.regnart@gmail.com>
2025-02-13 17:55:24 +0000

    testing minting and flipping mood on anvil chain

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -3,6 +3,7 @@
 import {Script} from "lib/forge-std/src/Script.sol";
 import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
 import {BasicNFT} from "src/BasicNFT.sol";
+import {MoodNFT} from "src/MoodNFT.sol";
 
 pragma solidity ^0.8.19;
 
@@ -20,6 +21,35 @@ contract MintBasicNFT is Script {
     function mintNftOnContract(address contractAddress) public {
         vm.startBroadcast();
         BasicNFT(contractAddress).mintNft(PUG);
+        vm.stopBroadcast();
+    }
+}
+
+contract MintMoodNft is Script {
+    function run() external {
+        address mostRecentlyDeployedMoodNft = DevOpsTools
+            .get_most_recent_deployment("MoodNFT", block.chainid);
+        mintNftOnContract(mostRecentlyDeployedMoodNft);
+    }
+
+    function mintNftOnContract(address moodNftAddress) public {
+        vm.startBroadcast();
+        MoodNFT(moodNftAddress).mintNft();
+        vm.stopBroadcast();
+    }
+}
+
+contract FlipMoodNft is Script {
+    uint256 public constant TOKEN_ID_TO_FLIP = 0;
+
+    function run() external {
+        address mostRecentlyDeployedMoodNft = DevOpsTools.get_most_recent_deployment("MoodNFT", block.chainid);
+        flipMoodNft(mostRecentlyDeployedMoodNft);
+    }
+
+    function flipMoodNft(address moodNftAddress) public {
+        vm.startBroadcast();
+        MoodNFT(moodNftAddress).flipMood(TOKEN_ID_TO_FLIP);
         vm.stopBroadcast();
     }
 }
